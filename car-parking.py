0aa6fd16939ba8e3a27204f383364e261101b372
Abhishek Tiwari <63968306+AbhishekTiwari07@users.noreply.github.com>
2022-03-18 00:26:19 +0530

    Delete car-parking.py

--- car-parking.py
+++
@@ -1,9 +0,0 @@
-import gym
-env = gym.make("Taxi-v3").env
-env.reset()
-env.render()
-
-print("Action Space {}".format(env.action_space))
-print("State Space {}".format(env.observation_space))
-
-env.close()
